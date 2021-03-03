fcb4f090c315b5582c313d86b300d24f6cc52b06
Mackan <52716811+vpg-code@users.noreply.github.com>
2021-03-03 07:44:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,5 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True)
